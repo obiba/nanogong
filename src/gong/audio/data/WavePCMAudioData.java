7d8ebf77420c431aefc967530b402e676127b402
Philippe Laflamme <philippe.laflamme@obiba.org>
2011-12-07 20:56:26 +0000

    Fixed playback issue when loading clip from a URL.

--- src/gong/audio/data/WavePCMAudioData.java
+++ src/gong/audio/data/WavePCMAudioData.java
@@ -2,11 +2,13 @@ package gong.audio.data;
 
 import gong.audio.AudioDataException;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.Enumeration;
 
 import javax.sound.sampled.AudioFileFormat;
@@ -16,10 +18,10 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class WavePCMAudioData extends BlockAudioData {
-	
+
 	private static final int BLOCK_SIZE_IN_SAMPLES = 4096;
 
-    public static final String FILE_EXTENSION = ".wav";
+	public static final String FILE_EXTENSION = ".wav";
 
 	public WavePCMAudioData(AudioFormat format) {
 		super(new AudioFormat(format.getSampleRate(), 16, 1, true, true));
@@ -29,7 +31,7 @@ public class WavePCMAudioData extends BlockAudioData {
 	public String getFileExtension() {
 		return FILE_EXTENSION;
 	}
-	
+
 	@Override
 	protected Block createBlock() {
 		return new WavePCMBlock();
@@ -42,20 +44,21 @@ public class WavePCMAudioData extends BlockAudioData {
 
 	@Override
 	public long getMemoryUsage() {
-		return getLength()*2;
+		return getLength() * 2;
 	}
 
 	@Override
 	public void sendToStream(OutputStream stream) throws IOException,
 			AudioDataException {
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream(getLength()*2);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(getLength() * 2);
 		Enumeration e = blockData.elements();
 		while (e.hasMoreElements()) {
 			WavePCMBlock block = (WavePCMBlock) e.nextElement();
 			block.sendToStream(baos);
 		}
-		AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(baos.toByteArray()), getFormat(), getLength());
+		AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(
+				baos.toByteArray()), getFormat(), getLength());
 		AudioSystem.write(ais, AudioFileFormat.Type.WAVE, stream);
 	}
 
@@ -64,14 +67,17 @@ public class WavePCMAudioData extends BlockAudioData {
 			throws IOException, AudioDataException {
 		try {
 			AudioInputStream ais = AudioSystem.getAudioInputStream(stream);
-			if(ais.getFormat().matches(getFormat()) == false) {
+			if (ais.getFormat().matches(getFormat()) == false) {
 				// transcode
 				ais = AudioSystem.getAudioInputStream(getFormat(), ais);
 			}
-			while(ais.available() > 0) {
-				WavePCMBlock block = new WavePCMBlock();
-				block.readFromStream(ais);
+			WavePCMBlock block = new WavePCMBlock();
+			boolean eof = block.readFromStream(ais);
+			super.blockData.add(block);
+			while (!eof) {
+				block = new WavePCMBlock();
 				super.blockData.add(block);
+				eof = block.readFromStream(ais);
 			}
 			super.availableBlocks = super.blockData.size();
 			super.position = getLength();
@@ -80,48 +86,64 @@ public class WavePCMAudioData extends BlockAudioData {
 		}
 
 	}
-	
+
 	private class WavePCMBlock extends Block {
-        
-        public WavePCMBlock() {
-            super(BLOCK_SIZE_IN_SAMPLES);
-        }
 
-        public synchronized int read() throws AudioDataException {
-            int dataIndex = position << 1;
-            int a = data[dataIndex] & 0x00FF;
-            int b = data[dataIndex + 1] & 0x00FF;
-            position++;
+		public WavePCMBlock() {
+			super(BLOCK_SIZE_IN_SAMPLES);
+		}
 
-            int sample =  a << 8 | b;
-            return sample;
-        }
+		public synchronized int read() throws AudioDataException {
+			int dataIndex = position << 1;
+			int a = data[dataIndex] & 0x00FF;
+			int b = data[dataIndex + 1] & 0x00FF;
+			position++;
 
-        public synchronized void write(int sample) throws AudioDataException {
-            if (eob()) throw new AudioDataException("Invalid write request.");
+			int sample = a << 8 | b;
+			return sample;
+		}
+
+		public synchronized void write(int sample) throws AudioDataException {
+			if (eob())
+				throw new AudioDataException("Invalid write request.");
+			if (data == null) {
+				data = new byte[size * 2];
+			}
+
+			int dataIndex = position << 1;
+			data[dataIndex] = (byte) ((sample >> 8) & 0xFF);
+			data[dataIndex + 1] = (byte) (sample & 0xFF);
+			position++;
+		}
+
+		public synchronized void sendToStream(OutputStream stream)
+				throws IOException, AudioDataException {
+			if (data == null)
+				throw new AudioDataException("Invalid send request.");
+			stream.write(data);
+		}
+
+		public synchronized boolean readFromStream(InputStream stream) throws IOException, AudioDataException {
             if (data == null) {
                 data = new byte[size * 2];
             }
-
-            int dataIndex = position << 1;
-            data[dataIndex] = (byte) ((sample >> 8) & 0xFF);
-            data[dataIndex+1] = (byte) (sample & 0xFF);
-            position++;
-        }
-
-        public synchronized void sendToStream(OutputStream stream) throws IOException, AudioDataException {
-            if (data == null) throw new AudioDataException("Invalid send request.");
-            stream.write(data);
-        }
-
-        public synchronized void readFromStream(InputStream stream) throws IOException, AudioDataException {
-            if (data == null) {
-                data = new byte[size * 2];
+            // Provides a read() method that will read as much as possible without blocking
+            BufferedInputStream bis = new BufferedInputStream(stream);
+            
+            // Read some data.
+            int read = bis.read(data, 0, data.length);
+            int length = read;
+            boolean eof = read < 0;
+            // If we haven't reached the end of the stream and we still have room to store samples, read some more
+            while(!eof && length < data.length) {
+            	read = bis.read(data, length, data.length - length);
+            	eof = read < 0;
+            	if(!eof) length += read;
             }
-            int read = stream.read(data);
-            position += read / 2;
+            // Place the position at the end (unless we didn't read anything
+        	position += length >= 0 ? length / 2 : 0;
+            return eof;
         }
-        
-    }
+	}
 
 }
